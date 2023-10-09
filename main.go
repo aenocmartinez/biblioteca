1461f5d7b97d0025f012d7da423dd46a12c9868b
abimelecmartinez <enoc.martinez@pulzo.com>
2023-10-08 19:10:00 -0500

    :sparkles: CORS

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func main() {
 
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery(), CORSMiddleware())
 
-	r.Use(gin.Logger(), gin.Recovery())
 	r.GET("books", controller.BookList)
 	r.GET("books/:id", controller.ReadBook)
 	r.POST("books", controller.CreateBook)
