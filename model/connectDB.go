0f4223521b3e97477fa0af28db5e2a55cdfb8f23
abimelecmartinez <enoc.martinez@pulzo.com>
2023-10-08 20:36:14 -0500

    :recycle: Conexion con base de datos Postgresql sslmode require

--- model/connectDB.go
+++ model/connectDB.go
@@ -39,7 +39,7 @@ func InstanceDB() *connectMySQL {
 
 		if instance == nil {
 
-			connStr := "host=" + host + " port=" + port + " user=%" + user + " " + "password=" + pass + " dbname=" + database + " sslmode=disable"
+			connStr := "host=" + host + " port=" + port + " user=%" + user + " " + "password=" + pass + " dbname=" + database + " sslmode=enable"
 			conn, err := sql.Open("postgres", connStr)
 			if err != nil {
 				fmt.Println(err)
